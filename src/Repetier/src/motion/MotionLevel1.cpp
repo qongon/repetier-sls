6da1c97a1c905555f3a9d3eee5101b803d4fa10d
Moses <moses.github@outlook.com>
2020-10-19 10:57:13 -0400

    Fix extra cold extrusion warnings on moves
    Small bug since we sometimes do a moveByX with the E Axis destination set to the current position instead of using IGNORE_COORDINATE. 
    Now just quickly checks if the destination is the same before warning.

--- src/Repetier/src/motion/MotionLevel1.cpp
+++ src/Repetier/src/motion/MotionLevel1.cpp
@@ -687,6 +687,7 @@ void Motion1::setTmpPositionXYZE(float x, float y, float z, float e) {
 
 // Move with coordinates in official coordinates (before offset, transform, ...)
 bool Motion1::moveByOfficial(float coords[NUM_AXES], float feedrate, bool secondaryMove) {
+    bool movingEAxis = (coords[E_AXIS] != currentPosition[E_AXIS]);
     Printer::unparkSafety();
     FOR_ALL_AXES(i) {
         if (coords[i] != IGNORE_COORDINATE) {
@@ -702,8 +703,8 @@ bool Motion1::moveByOfficial(float coords[NUM_AXES], float feedrate, bool second
 #endif
     ) { // ignore
 #if MIN_EXTRUDER_TEMP > MAX_ROOM_TEMPERATURE
-        if (coords[E_AXIS] != IGNORE_COORDINATE && !Printer::debugDryrun()) {
-            Com::printWarningFLN(Com::tColdExtrusionPrevented);
+        if (movingEAxis && coords[E_AXIS] != IGNORE_COORDINATE && !Printer::debugDryrun()) {
+            Com::printWarningFLN(Com::tColdExtrusionPrevented); 
         }
 #endif
         destinationPositionTransformed[E_AXIS] = currentPositionTransformed[E_AXIS];
@@ -869,6 +870,7 @@ void Motion1::setToolOffset(float ox, float oy, float oz) {
 
 // Move to the printer coordinates (after offset, transform, ...)
 bool Motion1::moveByPrinter(float coords[NUM_AXES], float feedrate, bool secondaryMove) {
+    bool movingEAxis = (coords[E_AXIS] != destinationPositionTransformed[E_AXIS]);
     Printer::unparkSafety();
     FOR_ALL_AXES(i) {
         if (coords[i] == IGNORE_COORDINATE) {
@@ -884,7 +886,7 @@ bool Motion1::moveByPrinter(float coords[NUM_AXES], float feedrate, bool seconda
 #endif
     ) {
 #if MIN_EXTRUDER_TEMP > MAX_ROOM_TEMPERATURE
-        if (coords[E_AXIS] != IGNORE_COORDINATE && !Printer::debugDryrun()) {
+        if (movingEAxis && coords[E_AXIS] != IGNORE_COORDINATE && !Printer::debugDryrun()) {
             Com::printWarningFLN(Com::tColdExtrusionPrevented);
         }
 #endif
