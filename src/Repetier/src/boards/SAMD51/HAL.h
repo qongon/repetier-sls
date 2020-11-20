644dfc7196abdb0cc679633febb6e201b3a65259
Moses <moses.github@outlook.com>
2020-11-20 18:27:45 -0500

    HAL - Add handlePeriodical
    We'll definitely need this in the future for HAL specific tasks. Adding it now.

--- src/Repetier/src/boards/SAMD51/HAL.h
+++ src/Repetier/src/boards/SAMD51/HAL.h
@@ -627,6 +627,7 @@ public:
         RFSERIAL.flush();
     }
     static void setupTimer();
+    static void handlePeriodical();
     static void updateStartReason();
     static void showStartReason();
     static int getFreeRam();
