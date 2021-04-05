f291910db5833251f7a514587892e8b18aad3c1e
Moses <moses.github@outlook.com>
2021-04-05 09:36:45 -0400

    Remove previous DueUSB disconnect fix
    Unnecessary with atmel's USB lib.

--- src/Repetier/src/communication/gcode.cpp
+++ src/Repetier/src/communication/gcode.cpp
@@ -1302,14 +1302,6 @@ int SerialGCodeSource::readByte() {
 }
 
 void SerialGCodeSource::writeByte(uint8_t byte) {
-#if defined(DUE_BOARD)
-    if (usbHostSource == this) {
-        if (!Is_udd_suspend()) {
-            stream->write(byte);
-        }
-        return;
-    }
-#endif
     stream->write(byte);
 }
 
